27d22e0c8c52a67e9851c0537ec0f164a8ef2348
puchk <kpuchalski@gmail.com>
2017-03-18 13:44:06 -0700

    queryURLs examples with api keys

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+// Example direction and places api with api key
+/*
+var directionsQueryURL = https://maps.googleapis.com/maps/api/directions/json?origin=Disneyland&destination=Universal+Studios+Hollywood4&key=AIzaSyDegJxVf_X3LfYHpbD6xnUJiy-3NXit8hI
+
+var placesQueryURL = https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyATyZWwhsd6B4zceXSDcl2bdQ-dJh0aFrk*/
