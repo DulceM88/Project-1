35bdaa89bf3c3aa8388700cd4bb38da216b34598
Dulce <00dukesup@gmail.com>
2017-03-18 13:20:22 -0700

    add firebase credentials

---
+++ JavaScript/app.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAUg1plSkJn11Oq01PNfhWTDUjfzO2GJoc",
+    authDomain: "along-the-way-161918.firebaseapp.com",
+    databaseURL: "https://along-the-way-161918.firebaseio.com",
+    storageBucket: "along-the-way-161918.appspot.com",
+    messagingSenderId: "250257772204"
+  };
+  firebase.initializeApp(config);
+
+
+  //Googleplaces call
+
